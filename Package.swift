6c1d5d4b10b5e4e882f1608a37558bc8bffa07a2
Kai Straßmann <derkai@gmail.com>
2024-09-13 18:45:41 +0200

    use swift 6 language mode

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
@@ -20,5 +20,6 @@ let package = Package(
             .product(name: "AsyncDNSResolver", package: "swift-async-dns-resolver"),
         ]),
         .testTarget(name: "RadioBrowserSwiftTests", dependencies: ["RadioBrowserSwift"]),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
